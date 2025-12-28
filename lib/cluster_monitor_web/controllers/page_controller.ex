7cbad0c43569370bcaebd05155502d7f4d5667ae
Nick Potts <github@nickpotts.com.au>
2025-12-28 11:19:47 +1100

    node count

--- lib/cluster_monitor_web/controllers/page_controller.ex
+++ lib/cluster_monitor_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule ClusterMonitorWeb.PageController do
   use ClusterMonitorWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    nodes = Node.list()
+    node_count = length(nodes) + 1
+
+    render(conn, :home, node: Node.self(), nodes: nodes, node_count: node_count)
   end
 end
